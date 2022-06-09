4c864b0d6228cc350f85a148c00a4f8cfa99ce64
jmiego95 <johnmiego@gmail.com>
2022-06-08 17:53:18 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+print("Welcome to Hell")
+prompt = input("Would you like to start the game. Y/N ").capitalize()
+if prompt == "Y":
+    print("The Game has began")
+else:
+    print("Invalid Response")
+
+print("Your head is throbbing. Dazed and confused you slowly get your bearing. You stumble upon a dagger and hammer")
+prompt2 = input("Which weapon would you like. Hammer or Dagger? ").capitalize()
+if prompt2 == "Hammer":
+    print("A dwarven hammer, you've chosen well")
+else:
+    print("You picked up rusty old dagger")
